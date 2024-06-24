d54c11b1fc5203491b2fffaa8832beae9914b3e3
joaonunez <joaovaldiglesias@gmail.com>
2024-06-24 00:16:17 -0400

    avance de modulacion en react

--- aplicacion/src/index.js
+++ aplicacion/src/index.js
@@ -1,14 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {Nav} from "./Tabs";
-import {Contenedor} from "./ContenedorTabs";
+import {Nav} from "./Nav";
+import {Ventas} from "./Ventas";
+import {Productos} from "./Productos"
 import {Footer} from "./Footer";
 
 
 
 
 ReactDOM.render(<Nav />, document.getElementById("nav"));
-ReactDOM.render(<Contenedor/>, document.getElementById("contenido"))
+ReactDOM.render(<Ventas/>, document.getElementById("ventas"));
+ReactDOM.render(<Productos/>, document.getElementById("productos"));
 ReactDOM.render(<Footer/>, document.getElementById("footer"))
 
 /* ReactDOM.render(<Productos/>, document.getElementById("myTabContent"));
